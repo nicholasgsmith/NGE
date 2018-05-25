9b9972da41ecb271f3e85144ce00cf390e33ccd1
Nicholas Smith <ns16699@bristol.as.uk>
2018-05-25 21:55:27 +0100

    Added main header and setup function

---
+++ NGE.h
@@ -0,0 +1,30 @@
+using namespace std;
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#ifndef NGE_H
+#define NGE_H
+
+//SDL Libaries
+#include "SDL.h"
+#include <SDL_opengl.h>
+#include <SDL_mixer.h>
+
+//Glut Libary
+#include "GL/freeglut.h"
+
+//DevIL Libary
+#include <IL/il.h>
+
+//Free-Type Fonts Libaries
+#include <ft2build.h> 
+#include FT_FREETYPE_H
+#include FT_BITMAP_H
+
+//Standard Libaries
+#include <ctime>
+#include <cstdlib>
+
+#endif
---
+++ NGE_Window.h
@@ -0,0 +1,8 @@
+#ifndef NGE_WIDOW_H
+#define NGE_WIDOW_H
+
+#include "NGE.h"
+
+int NGE_Setup();
+
+#endif
