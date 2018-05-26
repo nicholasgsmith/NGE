ff6ad71ed1a99a5b0792ee3d0ea6333986890d87
Nicholas Smith <ns16699@bristol.as.uk>
2018-05-26 17:24:06 +0100

    Added timers

--- NGE_Timing.cpp
+++ NGE_Timing.cpp
@@ -1,2 +1,64 @@
 #include "NGE_Timing.h"
 using namespace std;
+
+#include "NGE.h"
+
+using namespace std;
+
+NGE_Timer::NGE_Timer()
+{
+	startTime = SDL_GetTicks();
+	timePaused = 0;
+	pauseTime = 0;
+	paused = false;
+}
+
+int NGE_Timer::resetTimer()
+{
+	startTime = SDL_GetTicks();
+	timePaused = 0;
+	pauseTime = 0;
+	paused = false;
+	return 0;
+}
+
+int NGE_Timer::rollForwardTimer(int rollForwardAmount)
+{
+	//Adjust the timers start time accordingly
+	startTime += rollForwardAmount;
+	return 0;
+}
+
+int NGE_Timer::timePassed()
+{
+	//The time passed since the timer was turned on is the current time minus the time it was turned on minus any time it was paused for
+	return SDL_GetTicks() - startTime - getTimePaused();
+}
+
+int NGE_Timer::pauseTimer(bool pause)
+{
+	//If we want to pause, pause the timer and set the time it was paused to be now
+	if (pause && !paused)
+	{
+		pauseTime = SDL_GetTicks();
+		paused = true;
+	}
+	else if (!pause && paused) //If we want to unpause, unpause the timer and adjust the amount of time it was paused for accordingly
+	{
+		timePaused += (SDL_GetTicks() - pauseTime);
+		paused = false;
+	}
+
+	return 0;
+}
+
+int NGE_Timer::getTimePaused()
+{
+	//If the timer is currently paused, we need to include the current strecht of time it has been paused for
+	if (paused)
+	{
+		return timePaused + (SDL_GetTicks() - pauseTime);
+	}
+	
+	return timePaused;
+}
--- NGE_Timing.h
+++ NGE_Timing.h
@@ -5,4 +5,41 @@ using namespace std;
 
 #include "NGE_Master.h"
 
+//Creates a Timer which can be used to determine how much time has passed between events
+//All values are in milli-seconds
+class NGE_Timer
+{
+private:
+	int timePaused;
+	int pauseTime;
+	int startTime;
+	bool paused;
+
+public:
+	//Creates a new timer. Its default start time is whatever time it is now
+	NGE_Timer();
+
+	//Sets the timers start time to now, unpauses the timer and clears all pause-time logs
+	int resetTimer();
+
+	//Moves the time the timer was started forward by a certain amount
+	//You can pass a negative integer to move the start time backwards instead
+	int rollForwardTimer(int rollForwardAmount);
+
+	//Gets the amount of time since the start time of the timer that it hasnt been paused
+	//It can return a negative value if the start time was in the future, if set via rollForwardTimer
+	//Addtionally, since any time paused is deducted from the timer, if the time paused > (current time - start time) then it can also be negative
+	//Again, you would need to use rollForwardTimer to make this happen
+	int timePassed();
+
+	//Sets the timer to be paused or not
+	//While paused, the amount of time the timer records as having passed does not increase
+	int pauseTimer(bool paused);
+
+	//Returns the amount of time the timer has been paused
+	//This is deducted from the time amount when using timePassed
+	//It is reset by resetTimer
+	int getTimePaused();
+};
+
 #endif
