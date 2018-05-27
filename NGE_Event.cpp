540edae997e6c4863fb4bfc320354ce21e7c1063
nicholasgsmith <ns16699@bristol.ac.uk>
2018-05-27 13:48:15 +0100

    Added Event class and enums for input

--- NGE_Event.cpp
+++ NGE_Event.cpp
@@ -1,2 +1,16 @@
 #include "NGE_Event.h"
 using namespace std;
+
+//Constucts a blank new event
+NGE_Event::NGE_Event()
+{
+	source = NGE_SOURCE_NONE;
+	keyboardInput = NGE_KEYBOARDINPUT_NONE;
+	mouseButton = NGE_MOUSEBUTTON_NONE;
+	mouseEventType = NGE_MOUSEEVENTTYPE_NONE;
+	windowEventType = NGE_WINDOWEVENTTYPE_NONE;
+	keyboardEventType = NGE_KEYBOARDEVENTTYPE_NONE;
+	keyboardCharacter = '_';
+	timeStamp = 0;
+	cursor_x = 0;
+}
--- NGE_Event.h
+++ NGE_Event.h
@@ -2,8 +2,48 @@
 #define NGE_EVENT_H
 
 using namespace std;
-
 #include "NGE_Master.h"
+
+//Records which external source caused the event
+enum NGE_Source { NGE_SOURCE_NONE, NGE_SOURCE_MOUSE, NGE_SOURCE_KEYBOARD, NGE_SOURCE_WINDOW };
+
+//Records for keyboards and mice which input of them was used
+enum NGE_KeyboardInput { NGE_KEYBOARDINPUT_NONE, NGE_KEYBOARDINPUT_ENTER, NGE_KEYBOARDINPUT_BACKSPACE, NGE_KEYBOARDINPUT_TEXT, NGE_KEYBOARDINPUT_LEFT, NGE_KEYBOARDINPUT_RIGHT, NGE_KEYBOARDINPUT_DOWN, NGE_KEYBOARDINPUT_UP };
+enum NGE_MouseButton { NGE_MOUSEBUTTON_NONE, NGE_MOUSEBUTTON_RIGHT, NGE_MOUSEBUTTON_LEFT };
+
+//Records for each source exactly what the event was
+enum NGE_MouseEventType { NGE_MOUSEEVENTTYPE_NONE, NGE_MOUSEEVENTTYPE_RELEASE, NGE_MOUSEEVENTTYPE_PRESS, NGE_MOUSEEVENTTYPE_MOTION };
+enum NGE_WindowEventType { NGE_WINDOWEVENTTYPE_NONE, NGE_WINDOWEVENTTYPE_RESIZE, NGE_WINDOWEVENTTYPE_MAXIMISE, NGE_WINDOWEVENTTYPE_MINIMISE };
+enum NGE_KeyboardEventType { NGE_KEYBOARDEVENTTYPE_NONE, NGE_KEYBOARDEVENTTYPE_RELEASE, NGE_KEYBOARDEVENTTYPE_PRESS };
+
+//Stores details of an event e.g. external input
+class NGE_Event
+{
+public:
+	NGE_Event();
+
+	//Records the source of the event
+	NGE_Source source;
+
+	//Records specifically what part of the source caused the event
+	NGE_KeyboardInput keyboardInput;
+	NGE_MouseButton mouseButton;
+
+	//Records was the event
+	NGE_MouseEventType mouseEventType;
+	NGE_WindowEventType windowEventType;
+	NGE_KeyboardEventType keyboardEventType;
+
+	//For keyboard input, what character key was pressed
+	char keyboardCharacter;
+
+	//For mouse input, where was the cursor at the time
+	int cursor_x;
+	int cursor_y;
+
+	//When did the event happen
+	int timeStamp;
+};
 
 
 #endif
