a53197785104c1b099dd05a39ec1601519973e38
nicholasgsmith <ns16699@bristol.ac.uk>
2018-05-28 18:17:02 +0100

    Added Texture class

---
+++ NGE_Texture.h
@@ -0,0 +1,26 @@
+#ifndef NGE_TEXTURE_H
+#define NGE_TEXTURE_H
+
+using namespace std;
+
+#include "NGE_Master.h"
+
+class NGE_Texture
+{
+private:
+	int width_, height_;
+	GLuint textureID_;
+
+public:
+	NGE_Texture();
+	~NGE_Texture();
+
+	int setWidth(int width);
+	int getWidth();
+	int setHeight(int height);
+	int getHeight();
+	int getTextureID();
+	GLuint* getTextureIDAddress();
+};
+
+#endif
