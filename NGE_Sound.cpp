fe15a1710dd9c4b00b6042c62a91384b25c2154d
nicholasgsmith <ns16699@bristol.ac.uk>
2018-05-30 19:18:54 +0100

    Added comments

--- NGE_Sound.cpp
+++ NGE_Sound.cpp
@@ -24,9 +24,11 @@ int NGE_SoundBite::loadSoundBite(string file)
 {
 	ifstream canOpen(file);
 
+	//Confirms that a soundbite hasn't been loaded and that the file we are loading from exists
 	if (!soundLoaded && canOpen)
 	{
 		canOpen.close();
+		//Loads the soundbite from the file
 		soundBite = Mix_LoadWAV(file.c_str());
 		return 0;
 	}
@@ -41,6 +43,7 @@ int NGE_SoundBite::deleteSoundBite()
 {
 	if (soundLoaded)
 	{
+		//Clears the memory assigned to the soundbite
 		Mix_FreeChunk(soundBite);
 		soundLoaded = false;
 		return 0;
@@ -56,6 +59,7 @@ int NGE_SoundBite::playSound(int channel)
 {
 	if (soundLoaded)
 	{
+		//Plays the soundbite on the provided channel
 		Mix_PlayChannel(channel, soundBite, 0);
 		return 0;
 	}
--- NGE_Sound.h
+++ NGE_Sound.h
@@ -15,11 +15,23 @@ private:
 	Mix_Chunk *soundBite;
 
 public:
+	//Creates a new, blank soundbite instance
 	NGE_SoundBite();
+
+	//Creates a new soundbite instance and attempts to load a soundbite from the provided file into it
 	NGE_SoundBite(string file);
+
+	//Returns whether the instance has had a soundbite loaded into it
 	bool isSoundLoaded();
+
+	//Loads a soundbite from a file into the instance
 	int loadSoundBite(string file);
+
+	//Deletes a loaded soundbite data from the instance
+	//The instance will still exists after, but contain no data
 	int deleteSoundBite();
+
+	//Plays a soundbite if one has been loaded
 	int playSound(int channel);
 };
 
