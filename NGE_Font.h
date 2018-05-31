0e820588bfef8579bdd584176c619af1d2fe6ed7
nicholasgsmith <ns16699@bristol.ac.uk>
2018-05-31 13:36:56 +0100

    Added font class

--- NGE_Font.h
+++ NGE_Font.h
@@ -5,4 +5,23 @@ using namespace std;
 
 #include "NGE_Master.h"
 
+#include <string>
+
+class NGE_Font
+{
+private:
+	FT_Face face;
+	FT_Glyph_Metrics metrics[256];
+	GLubyte* characterData[255];
+	GLuint charHeight[255], charWidth[255], charYBearing[255], charDownYBearing[255];
+	FT_Library libary;
+	int textSize, largestWidth, largestHeight, largestYBearing, largestDownYBearing;
+
+	NGE_Font();
+	NGE_Font(string file, int textSize);
+	~NGE_Font();
+	int deleteFont();
+	int loadFont(string file, int textSize);
+};
+
 #endif
