08a33d98b7a86a20b2de443cfc57d994ec42d734
nicholasgsmith <ns16699@bristol.ac.uk>
2018-05-31 11:33:02 +0100

    Added channel functions

--- NGE_SoundChannel.cpp
+++ NGE_SoundChannel.cpp
@@ -4,3 +4,22 @@ using namespace std;
 #include "NGE.h"
 
 using namespace std;
+
+int pauseChannel(bool pause, int channel)
+{
+	if (!pause)
+	{
+		Mix_Resume(channel);
+	}
+	else
+	{
+		Mix_Pause(channel);
+	}
+	return 0;
+}
+
+int stopChannel(int channel)
+{
+	Mix_HaltChannel(channel);
+	return 0;
+}
