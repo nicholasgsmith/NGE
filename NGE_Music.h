2993be8eec7f2ea18422ee1fa846df3eab223a2e
nicholasgsmith <ns16699@bristol.ac.uk>
2018-05-31 11:10:59 +0100

    Added music class

--- NGE_Music.h
+++ NGE_Music.h
@@ -5,4 +5,24 @@ using namespace std;
 
 #include "NGE_Master.h"
 
+#include <string>
+
+class NGE_Music
+{
+private:
+	bool musicPlaying;
+	bool musicLoaded;
+	Mix_Music *music;
+
+public:
+	NGE_Music();
+	NGE_Music(string file);
+	~NGE_Music();
+	int loadMusic(string filename, string ID);
+	int playMusic(string ID, bool loopMusic);
+	int pauseMusic(bool pause);
+	int stopMusic();
+	int deleteMusic(string ID);
+};
+
 #endif
