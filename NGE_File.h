f2a1e119d33da64174950b48bfe8dd88c992777b
nicholasgsmith <ns16699@bristol.ac.uk>
2018-05-31 18:14:35 +0100

    Added file class

--- NGE_File.h
+++ NGE_File.h
@@ -5,4 +5,36 @@ using namespace std;
 
 #include "NGE_Master.h"
 
+#include <string>
+#include <fstream>
+
+class NGE_File
+{
+private:
+	bool fileOpen;
+	bool newline;
+	string filename;
+	string seperator;
+
+public:
+	NGE_File();
+	NGE_File(string file);
+	int openFile(string file);
+	int createFile(string file);
+	int deleteFile();
+	int wipeFile();
+	int setSeperator(string seperator);
+	string getSeperator();
+	bool doesFileExist(string file);
+	int linesInFile();
+	int itemsInLine(int line);
+	int appendInt(int data);
+	int appendDouble(double data);
+	int appendString(string data);
+	int appendNewline();
+	int readInt(int line, int item);
+	int readDouble(int line, int item);
+	int readString(int line, int item);
+};
+
 #endif
