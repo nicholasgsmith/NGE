548dd3c7ce1611cc0d996002bbe78772751b1cd5
nicholasgsmith <ns16699@bristol.ac.uk>
2018-05-31 11:32:06 +0100

    Added channel function declarations

--- NGE_SoundChannel.h
+++ NGE_SoundChannel.h
@@ -1,8 +1,15 @@
-#ifndef NGE_SOUNDBITE_H
-#define NGE_SOUNDBITE_H
+#ifndef NGE_SOUNDCHANNEL_H
+#define NGE_SOUNDCHANNEL_H
 
 using namespace std;
 
 #include "NGE_Master.h"
+
+//Pauses or unpauses a given channel based off the pause parameter
+//Pausing stops any sounds playing on the channel
+int pauseChannel(bool pause, int channel);
+
+//Stops a given channel, stopping all sounds playing on it
+int stopChannel(int channel);
 
 #endif
