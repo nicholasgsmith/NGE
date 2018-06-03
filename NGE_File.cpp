f55aace3e3bc6d983e0362a0ae7b9a8ead28b5a6
nicholasgsmith <ns16699@bristol.ac.uk>
2018-06-03 15:55:12 +0100

    Commeneted file things

--- NGE_File.cpp
+++ NGE_File.cpp
@@ -19,6 +19,7 @@ NGE_File::NGE_File(string file)
 {
 	NGE_File();
 
+	//Opens the file if it exists, or creates it and then opens it otherwise
 	if (doesFileExist(file))
 	{
 		openFile(file);
@@ -31,8 +32,10 @@ NGE_File::NGE_File(string file)
 
 int NGE_File::openFile(string file)
 {
+	//You can only open the file if it exists and a file is not already open
 	if (doesFileExist(file) && !fileOpen)
 	{
+		//Record that the file is open and its path
 		filename = file;
 		fileOpen = true;
 
@@ -41,13 +44,19 @@ int NGE_File::openFile(string file)
 		string currentItem = "";
 		signed int i = 0, j = 0, k = 0;
 
+		//Loop through and load each line of the file
 		while (getline(reader, line, '\n'))
 		{
+			//Create a new vector for each lines items
 			fileContents.push_back(vector<string>());
+
+			//The limit that which upon reaching e dont need to check any more characters in the line
 			int limit = line.size() - endSeperator.size();
 
+			//Go through each character, ignoring the start and end characters
 			for (j = startSeperator.size(); j < limit; j++)
 			{	
+				//If possible, check if the next sequence of characters starting from the current one are a seperator
 				if (j + seperator.size() < line.size())
 				{
 					for (k = 0; k < seperator.size(); k++)
@@ -58,22 +67,29 @@ int NGE_File::openFile(string file)
 						}
 					}
 				}
+				//This will be true if the current character is the start of a seperator
 				if (k == seperator.size())
 				{
+					//Add the current item to the item list and reset the item recorder
 					fileContents[i].push_back(currentItem);
 					currentItem = "";
+					//Ignore the characters that make up the seperator
 					j += k - 1;
-					k = 0;
 				}
 				else
 				{
+					//If the current character is not part of a seperator ass it to the current item recording
 					currentItem = currentItem + line.at(j);
 				}
 			}
+			//At the end of a line we need to add the final item, as it wasnt recorded before since it doesnt end in a seperator
+			//The exception to this is if the line size if 0, in which case it was an emepty line and we add nothing
+			//We can just check if the currentitem is "" as an item may be the empty string
 			if (line.size() != 0)
 			{
 				fileContents[i].push_back(currentItem);
 			}
+			//Reset for the next line
 			currentItem = "";
 			i++;
 		}
@@ -92,22 +108,32 @@ int NGE_File::saveFile()
 {
 	if (fileOpen)
 	{
+		//Wipe the file so we can re-add all the information
+		wipeFile();
+
 		ofstream saver(filename);
 		int lines = linesInFile();
 
+		//Go through each line
 		for (int i = 0; i < lines; i++)
 		{
+			//Check that there is at least 1 item in the line
+			//If there isnt, we dont include the start and stop seperators
 			if (fileContents[i].size() > 0)
 			{
+				//Add the start seperator
 				saver << startSeperator;
+				//Add each item to the file
 				for (int j = 0; j != fileContents[i].size(); j++)
 				{
 					saver << fileContents[i][j];
+					//If athe item is no the final item, add a seperator after it
 					if (j + 1 != fileContents[i].size())
 					{
 						saver << seperator;
 					}
 				}
+				//Add the stop seperator
 				saver << endSeperator;
 			}
 			saver << '\n';
@@ -139,11 +165,13 @@ int NGE_File::closeFile()
 
 int NGE_File::createFile(string file, bool openFile)
 {
+	//Only create the file if it doesnt already exist
 	if (!doesFileExist(file))
 	{
 		ofstream createFile(file);
 		createFile.close();
 
+		//Open the file for editing as well if requested
 		if (openFile)
 		{
 			this->openFile(file);
@@ -175,6 +203,7 @@ int NGE_File::wipeFile()
 {
 	if (fileOpen)
 	{
+		//To wipre the file we just delete and then recreate it
 		deleteFile();
 		createFile(filename, true);
 		return 0;
@@ -240,6 +269,7 @@ int NGE_File::addNewline()
 {
 	if (fileOpen)
 	{
+		//To add a new line we add a blank vector to out file contenst vector
 		fileContents.push_back(vector<string>());
 		return 0;
 	}
@@ -275,6 +305,7 @@ int NGE_File::appendItem(string data, int lineNumber)
 	{
 		if (lineNumber >= 0 && lineNumber < linesInFile())
 		{
+			//To add a new item just append a new string to the vector corresponding to the line
 			fileContents[lineNumber].push_back(data);
 			return 0;
 		}
@@ -315,6 +346,7 @@ int NGE_File::deleteItem(int lineNumber, int itemNumber)
 	{
 		if (lineNumber >= 0 && lineNumber < linesInFile() && itemNumber >= 0 && itemNumber < itemsInLine(lineNumber))
 		{
+			//We erase an element of the lines vector to delete an item
 			fileContents[lineNumber].erase(fileContents[lineNumber].begin() + itemNumber);
 			return 0;
 		}
@@ -335,6 +367,7 @@ int NGE_File::deleteLine(int lineNumber)
 	{
 		if (lineNumber >= 0 && lineNumber < linesInFile())
 		{
+			//We delete the vector corresponding to a line to delete the lines
 			fileContents.erase(fileContents.begin() + lineNumber);
 			return 0;
 		}
--- NGE_File.h
+++ NGE_File.h
@@ -21,26 +21,74 @@ private:
 	vector< vector <string> > fileContents;
 
 public:
+	//Creates a new blank instance for file manipulation
 	NGE_File();
+
+	//Creates a new instance for file manipulation and loads the given file into it
+	//If the file does not exist it creates it first before loading it
 	NGE_File(string file);
+
+	//Opens a file for manipulation
+	//This does not edit the contents of the file
+	//All methods which do not take a file but edit a files contents require this function to be called beforehand 
 	int openFile(string file);
+
+	//Saves the current file which was loaded using openFile
+	//Any changes are saved as well
+	//This does not close the file, you can continue to edit it without re-opening it
 	int saveFile();
+
+	//Closes the currently open file
+	//This does not save changes, saveFile shoudl be called beforehand unless you dont want changes saved
 	int closeFile();
+
+	//Creates a new blank file
+	//Also opens the file if requested
 	int createFile(string file, bool openFile);
+
+	//Deletes the currently open file
+	//Also closes it
 	int deleteFile();
+
+	//Wipes the contents of a file
+	//This does not close the file
 	int wipeFile();
+
+	//Returns what is currently being used as the seperator in files
 	string getSeperator();
+
+	//Returns true if a file exists and false otherwise
 	bool doesFileExist(string file);
 
+	//Returns the number of the lines in the currently open file
 	int linesInFile();
+
+	//Returns the number of items in a given line
 	int itemsInLine(int line);
 
+	//Adds a new line to the file
 	int addNewline();
+
+	//Appends the given string as a new item to the end of a line
+	//Returns -2 if a file is not open and -1 if your line does not exist
 	int appendItem(string data, int lineNumber);
+
+	//Appends the given string as a new item to the end of a line
+	//Returns -2 if a file is not open and -1 if your line does not exist
 	int readItem(string& data, int lineNumber, int itemNumber);
+
+	//Edits the given item to the previded string
+	//Returns -2 if a file is not open and -1 if your line or item does not exist
 	int editItem(string data, int lineNumber, int itemNumber);
+
+	//Deletes the given item
+	//Returns -2 if a file is not open and -1 if your line or item does not exist
 	int deleteItem(int lineNumber, int itemNumber);
+
+	//Deletes the given line
+	//Returns -2 if a file is not open and -1 if your line does not exist
 	int deleteLine(int lineNumber);
+
 };
 
 #endif
